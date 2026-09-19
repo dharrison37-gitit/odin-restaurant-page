1ad9cde4fd49edb417fe09a0e955d933ae13bfb6
dharrison37-gitit <308913916+dharrison37-gitit@users.noreply.github.com>
2026-09-19 00:01:17 -0500

    Adjust webpack configs for dev and production
    Installed webpack merge and created the common,
    development, and production config files.
    
    Update package.json to have script commands to run
    for dev and prod builds.

--- webpack.common.js
+++ webpack.common.js
@@ -1,9 +1,11 @@
 import path from "node:path";
+import { fileURLToPath } from "node:url";
 import HtmlWebpackPlugin from "html-webpack-plugin";
 
 export default {
-    mode: "development",
-    entry: "./src/index.js",
+    entry: {
+        app: "./src/index.js",
+    },
     output: {
         filename: "main.js",
         path: path.resolve(import.meta.dirname, "dist"),
